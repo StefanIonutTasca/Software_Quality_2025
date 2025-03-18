9bb4fbd47e0f48aa47e60ae5e5bc81efacba1272
StefanIonutTasca <StefanIonutTasca@users.noreply.github.com>
2025-03-18 15:03:13 +0000

    style: Format Java code with Google Java Format

--- src/test/java/org/jabberpoint/src/PresentationTest.java
+++ src/test/java/org/jabberpoint/src/PresentationTest.java
@@ -7,25 +7,25 @@ import org.junit.jupiter.api.Test;
 
 public class PresentationTest {
 
-    private org.jabberpoint.src.Presentation presentation;
-    
-    @BeforeEach
-    public void setUp() {
-        presentation = new org.jabberpoint.src.Presentation();
-        presentation.setTitle("Test Presentation");
-    }
-    
-    @Test
-    public void testGetTitle() {
-        assertEquals("Test Presentation", presentation.getTitle());
-    }
-    
-    @Test
-    public void testSlideManagement() {
-        int initialCount = presentation.getSize();
-        org.jabberpoint.src.Slide slide = new org.jabberpoint.src.Slide();
-        slide.setTitle("New Slide");
-        presentation.append(slide);
-        assertEquals(initialCount + 1, presentation.getSize());
-    }
+  private org.jabberpoint.src.Presentation presentation;
+
+  @BeforeEach
+  public void setUp() {
+    presentation = new org.jabberpoint.src.Presentation();
+    presentation.setTitle("Test Presentation");
+  }
+
+  @Test
+  public void testGetTitle() {
+    assertEquals("Test Presentation", presentation.getTitle());
+  }
+
+  @Test
+  public void testSlideManagement() {
+    int initialCount = presentation.getSize();
+    org.jabberpoint.src.Slide slide = new org.jabberpoint.src.Slide();
+    slide.setTitle("New Slide");
+    presentation.append(slide);
+    assertEquals(initialCount + 1, presentation.getSize());
+  }
 }
--- src/test/java/org/jabberpoint/src/SlideTest.java
+++ src/test/java/org/jabberpoint/src/SlideTest.java
@@ -7,23 +7,23 @@ import org.junit.jupiter.api.Test;
 
 public class SlideTest {
 
-    private org.jabberpoint.src.Slide slide;
-    
-    @BeforeEach
-    public void setUp() {
-        slide = new org.jabberpoint.src.Slide();
-        slide.setTitle("Test Slide");
-    }
-    
-    @Test
-    public void testGetTitle() {
-        assertEquals("Test Slide", slide.getTitle());
-    }
-    
-    @Test
-    public void testAppendText() {
-        int initialSize = slide.getSize();
-        slide.append(1, "Test Item");
-        assertEquals(initialSize + 1, slide.getSize());
-    }
+  private org.jabberpoint.src.Slide slide;
+
+  @BeforeEach
+  public void setUp() {
+    slide = new org.jabberpoint.src.Slide();
+    slide.setTitle("Test Slide");
+  }
+
+  @Test
+  public void testGetTitle() {
+    assertEquals("Test Slide", slide.getTitle());
+  }
+
+  @Test
+  public void testAppendText() {
+    int initialSize = slide.getSize();
+    slide.append(1, "Test Item");
+    assertEquals(initialSize + 1, slide.getSize());
+  }
 }
