c31a3d02cb97902c88bafd65b6041ec9df0b5933
StefanIonutTasca <stefan.tasca@student.nhlstenden.com>
2025-03-25 13:40:01 +0100

    Adding the KeyController Pattern

---
+++ src/main/java/org/jabberpoint/src/Command.java
@@ -0,0 +1,9 @@
+/**
+ * Command pattern interface for actions that can be performed on a presentation
+ */
+public interface Command {
+    /**
+     * Executes the command
+     */
+    void execute();
+}
