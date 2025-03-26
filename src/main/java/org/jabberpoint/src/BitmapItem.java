d15a1856ce9ae17f966f4ad3ddc1c7e0a3723921
radudumitrache <radu.dumitrache@student.nhlstenden.com>
2025-03-26 15:09:00 +0200

    tried to revert to see smth

--- src/main/java/org/jabberpoint/src/BitmapItem.java
+++ src/main/java/org/jabberpoint/src/BitmapItem.java
@@ -5,6 +5,7 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 import java.io.File;
+import java.io.InputStream;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
